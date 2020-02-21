9b185eca641327efdd3c72823fe45fdecfbc9001
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-02-21 13:24:08 +1100

    seeding modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,37 @@ url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 cocktails_serialized = open(url).read
 cocktail = JSON.parse(cocktails_serialized)
 
-24.times do
+# Ingredient.destroy_all!
+# Cocktail.destroy_all!
+
+32.times do
   Ingredient.create(name: cocktail['drinks'].sample['strIngredient1'])
 end
 
-puts 'Created 24 ingredients'
+puts "Created #{Ingredient.all.length}"
 
-cocktails = %w[Spritz Mojito Sangria Margarita LongIsland Punch BlueLagoon GinTo PinaColada]
+Cocktail.create!(name: "Spritz", image_url: "https://images.unsplash.com/photo-1570598912132-0ba1dc952b7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80")
 
-cocktails.each do |c|
-  Cocktail.create(name: c)
-end
+Cocktail.create!(name: "Old Fashioned", image_url: "https://images.unsplash.com/photo-1560179304-6fc1d8749b23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80")
 
-puts "Created #{cocktails.length} cocktails"
+Cocktail.create!(name: "Gin & Tonic", image_url: "https://images.unsplash.com/photo-1545438102-799c3991ffb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80")
+
+Cocktail.create!(name: "Margarita", image_url: "https://images.unsplash.com/photo-1486428263684-28ec9e4f2584?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80")
+
+Cocktail.create!(name: "Watermelon Caïpirinha", image_url: "https://images.unsplash.com/photo-1527661591475-527312dd65f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=716&q=80")
+
+Cocktail.create!(name: "Berry Fizz", image_url: "https://images.unsplash.com/photo-1527091755570-1daada653420?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=886&q=80")
+
+Cocktail.create!(name: "Blue Lagoon", image_url: "https://images.unsplash.com/photo-1559842623-b82d2e1228a5?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80")
+
+Cocktail.create!(name: "Mojito", image_url: "https://images.unsplash.com/photo-1551538827-9c037cb4f32a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=802&q=80")
+
+Cocktail.create!(name: "Cosmopolitan", image_url: "https://images.unsplash.com/photo-1559842438-2942c907c8fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80")
+
+Cocktail.create!(name: "Frozen Test", image_url: "https://images.unsplash.com/photo-1497534446932-c925b458314e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=814&q=80")
+
+Cocktail.create!(name: "Pina Colada", image_url: "https://images.unsplash.com/photo-1577595166653-c4b06b30fa18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80")
+
+Cocktail.create!(name: "Goyabakka", image_url: "https://images.unsplash.com/photo-1555940726-1c297abcc1f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80")
+
+puts "Created #{Cocktail.all.length}"
